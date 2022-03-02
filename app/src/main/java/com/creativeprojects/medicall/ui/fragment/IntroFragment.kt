868b470aa6caa23135871aee8cd880d5903f2f0b
Bunyad Mecidzade <the.bunyad@gmail.com>
2022-03-02 20:24:49 +0400

    [MED-4] Problem fixed

---
+++ app/src/main/java/com/creativeprojects/medicall/ui/fragment/IntroFragment.kt
@@ -0,0 +1,28 @@
+package com.creativeprojects.medicall.ui.fragment
+
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.creativeprojects.medicall.databinding.FragmentIntroBinding
+
+class IntroFragment : Fragment() {
+    private lateinit var binding: FragmentIntroBinding
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentIntroBinding.inflate(inflater, container, false)
+        Handler(Looper.getMainLooper()).postDelayed(
+            {
+                findNavController().navigate(IntroFragmentDirections.actionIntroFragmentToMainActivity())
+            }, 2000
+        )
+        return binding.root
+    }
+}
