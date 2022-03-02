088c3881eb43eb42b0bb756667bf7b387e7d3f25
Bunyad Mecidzade <the.bunyad@gmail.com>
2022-03-02 20:10:25 +0400

    [MED-4] first commit

--- app/src/main/java/com/creativeprojects/medicall/MainActivity.kt
+++ app/src/main/java/com/creativeprojects/medicall/MainActivity.kt
@@ -2,10 +2,13 @@ package com.creativeprojects.medicall
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.creativeprojects.medicall.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        var binding: ActivityMainBinding
     }
 }
