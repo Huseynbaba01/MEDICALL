7f76f01fb6f996b72b41b6019c10a43ef2f0ecef
Bunyad Mecidzade <the.bunyad@gmail.com>
2022-03-08 17:14:28 +0400

    [MED-11] diseases page

--- app/src/main/java/com/creativeprojects/medicall/ui/fragment/SelectUserTypeFragment.kt
+++ app/src/main/java/com/creativeprojects/medicall/ui/fragment/SelectUserTypeFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.creativeprojects.medicall.databinding.FragmentSelectUserTypeBinding
 
 
@@ -17,7 +18,7 @@ class SelectUserTypeFragment : Fragment() {
     ): View {
         binding = FragmentSelectUserTypeBinding.inflate(inflater)
         binding.cvCallAmbulance.setOnClickListener {
-
+            findNavController().navigate(SelectUserTypeFragmentDirections.actionSelectUserTypeFragmentToDiseasesFragment())
         }
         return binding.root
 
