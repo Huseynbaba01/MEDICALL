4d07df344b91e63c077e222b28f6efd335861f9e
Huseynbaba <hrzaquliyev@std.beu.edu.az>
2022-03-14 12:51:17 +0400

    [v1.0] test

--- app/src/main/java/com/creativeprojects/medicall/ui/activity/MainActivity.kt
+++ app/src/main/java/com/creativeprojects/medicall/ui/activity/MainActivity.kt
@@ -8,6 +8,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
